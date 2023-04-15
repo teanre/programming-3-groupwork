931b5186f1e543b7718f33ef33c78698975398f9
Terhi Rees <terhi.rees@tuni.fi>
2023-04-15 13:37:35 +0300

    update Student class to keep track which user is using app currently and their completed courses

--- Sisu/src/main/java/fi/tuni/prog3/sisu/Student.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Student.java
@@ -16,6 +16,13 @@ public class Student {
     @SerializedName("startingYear")
     public int startingYear;
     
+    //to keep track of their completed courses
+    public ArrayList<Course> completedCourses = new ArrayList<>();
+    
+    //keeps track of who is using the app right now
+    private static Student currentStudent;
+    
+    
     public Student(String name, String studentNumber, int startingYear) {
         this.name = name;
         this.studentNumber = studentNumber;
@@ -32,5 +39,25 @@ public class Student {
     
     public int getStartingYear() {
         return startingYear;
+    }
+    
+    public void addCompletedCourse(Course course) {
+        completedCourses.add(course);
+    }
+    
+    public ArrayList<Course> getCompletedCourses() {
+        return completedCourses;
+    }
+    
+    public void removeCompletedCourse(Course course) {
+        completedCourses.remove(course);
+    }
+    
+    public static Student getCurrentStudent() {
+        return currentStudent;
+    }
+    
+    public static void setCurrentStudent(Student student) {
+        currentStudent = student;
     }
 }

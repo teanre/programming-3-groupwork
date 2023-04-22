bbab83c809cf76e16fd4bef3675c2523bb1e7ed5
Terhi Rees <terhi.rees@tuni.fi>
2023-04-22 11:15:28 +0300

    remove unnecessary isStudentInFile method from FileProcessor

--- Sisu/src/main/java/fi/tuni/prog3/sisu/FileProcessor.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/FileProcessor.java
@@ -20,59 +20,18 @@ import java.util.ArrayList;
  */
 public class FileProcessor implements iReadAndWriteToFile {
     
+    /**
+     * Constructor
+     */
     public FileProcessor(){
         
     }
     
     /**
-     * Checks if the current user has used the programme before. If they have, 
-     * updates 
-     * @param fileName
-     * @param student
-     * @return
-     * @throws Exception 
-     */
-    public boolean isStudentInFile(String fileName, Student student) throws Exception {
-        try (Reader reader = new FileReader(fileName)) {
-            // read the file
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
-            // get the students array
-            JsonArray students = jsonObject.getAsJsonArray("students");
-                        
-            if (students != null) {
-
-                for (JsonElement studentElement : students) {
-                    JsonObject studentObject = studentElement.getAsJsonObject();
-                    String studentNr = studentObject.get("studentNumber").getAsString();
-
-                    if(studentNr.equals(student.getStudentNumber())) {
-                        //add the completedCourses data from json to this student's completedCourses attribute
-                        JsonArray courseArray = studentObject.getAsJsonArray("completedCourses");
-                        Gson gson2 = new GsonBuilder().setPrettyPrinting().create();
-                        Type type = new TypeToken<ArrayList<Course>>(){}.getType();
-                        ArrayList<Course> completedCourses =  gson2.fromJson(courseArray, type);
-                        
-                        //clear and add the completedCourses data
-                        student.completedCourses.clear();
-                        student.completedCourses.addAll(completedCourses);
-                        return true;
-                    }
-                }
-            }
-        } catch (IOException e) {
-                System.out.println(e);
-                return false;
-        }
-                
-        return false;
-    }
-    
-    /**
-     * 
-     * @param fileName
-     * @param student
-     * @return
+     * Saves a new users data to json file
+     * @param fileName, String, name of the file where the data is stored
+     * @param student, Student object 
+     * @return true if successfull, false otherwise
      * @throws Exception 
      */
     public boolean addStudentToFile(String fileName, Student student) throws Exception {
@@ -97,14 +56,13 @@ public class FileProcessor implements iReadAndWriteToFile {
             System.out.println(e);
             return false;
         }
-
         return true;
     }
     
     /**
-     * 
-     * @param fileName
-     * @return
+     * Creates a new file to store user data if it does not exist
+     * @param fileName String, name of source file
+     * @return boolean, true if successful, false otherwise
      * @throws Exception 
      */
     public boolean createNewFile(String fileName) throws Exception {
@@ -123,9 +81,9 @@ public class FileProcessor implements iReadAndWriteToFile {
     }
     
     /**
-     * updates the userdata json file with current user's info while quitting the programme
-     * @param fileName
-     * @return
+     * Updates the userdata json file with current user's info 
+     * @param fileName the source json file's name
+     * @return true if successful, false if not
      * @throws Exception 
      */
     @Override
@@ -150,7 +108,7 @@ public class FileProcessor implements iReadAndWriteToFile {
                 if (studentNr.equals(currentStudent.getStudentNumber())) {
                     //get theri compcourseslist json the jsonfile
                     studentJson = studentObject;
-                    //tiedoston sulkeminenkiii
+                    reader.close();
                     break;
                 }
             }                       
@@ -185,6 +143,13 @@ public class FileProcessor implements iReadAndWriteToFile {
     
     }
 
+    /**
+     * Reads current user's data from the json file and stores necessary info as 
+     * Student and Course objects 
+     * @param fileName, string, name of source file
+     * @return true if successful, false if not
+     * @throws Exception 
+     */
     @Override
     public boolean readFromFile(String fileName) throws Exception {
         try (Reader reader = new FileReader(fileName)) {
@@ -193,32 +158,32 @@ public class FileProcessor implements iReadAndWriteToFile {
             JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
             // get the students array
             JsonArray studentsArray = jsonObject.getAsJsonArray("students");
-
+            Student user = Student.getCurrentStudent();
             if (studentsArray != null) {
                 for (JsonElement studentElement : studentsArray) {
                     JsonObject studentObject = studentElement.getAsJsonObject();
-                    String name = studentObject.get("name").getAsString();
+                   // String name = studentObject.get("name").getAsString();
                     String studentNumber = studentObject.get("studentNumber").getAsString();
-                    int startingYear = studentObject.get("startingYear").getAsInt();
+                    if (studentNumber.equals(user.getStudentNumber())) { //we have found current user's data
+                        JsonArray coursesArray = studentObject.getAsJsonArray("completedCourses");
+                        ArrayList<Course> completedCourses = new ArrayList<>();
 
-                    JsonArray coursesArray = studentObject.getAsJsonArray("completedCourses");
-                    ArrayList<Course> completedCourses = new ArrayList<>();
-                    //String name, String id, String groupId, int minCredits
-                    for (JsonElement courseElement : coursesArray) {
-                        JsonObject courseObject = courseElement.getAsJsonObject();
-                        String courseName = courseObject.get("name").getAsString();
-                        String id  = courseObject.get("id").getAsString();
-                        String groupId = courseObject.get("groupId").getAsString();
-                        int minCredits = courseObject.get("minCredits").getAsInt();
-                        completedCourses.add(new Course(courseName, id, groupId, minCredits));
+                        for (JsonElement courseElement : coursesArray) {
+                            JsonObject courseObject = courseElement.getAsJsonObject();
+                            String courseName = courseObject.get("name").getAsString();
+                            String id  = courseObject.get("id").getAsString();
+                            String groupId = courseObject.get("groupId").getAsString();
+                            int minCredits = courseObject.get("minCredits").getAsInt();
+                            completedCourses.add(new Course(courseName, id, groupId, minCredits,
+                                                "","","","","")); //the other attributes not necessary for completedcourses data
+                        }
+                        for (var c : completedCourses) {
+                            user.addCompletedCourse(c);
+                        }
+                        break; // no need to iterate array further
                     }
-
-                    Student student = new Student(name, studentNumber, startingYear);
-                    student.completedCourses = completedCourses;
-
                 }
-            }           
-            
+            }                      
         } catch (IOException e) {
             System.out.println(e);
             return false;
